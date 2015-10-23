14aad3e8d0ec69fc575b1d2a7f945a24d3fd76d6
Your Name <josefdaly@gmail.com>
2015-10-23 13:44:45 -0700

    Tile placeholders properly rendering in grid

--- application.js
+++ application.js
@@ -1,19 +1,57 @@
 (function(){
+  "use strict";
   window.Minesweeper = window.Minesweeper || {};
 
-  var Game = Minesweeper.Game = React.createClass({
+  var GameComponent = React.createClass({
     getInitialState: function() {
-      return { board: [], gameOver: false, gameWon: false }
+      var board = new Minesweeper.Board(10, 10);
+      return { board: board, gameOver: false, gameWon: false }
     },
     updateGame: function() {
-
+      //logic will go here
     },
     render: function() {
       return (
-        <Board board={this.state.board} update={this.updateGame} />
+        <BoardComponent board={this.state.board} update={this.updateGame} />
       );
     }
   });
+
+  var BoardComponent = React.createClass({
+    render: function() {
+      var rows = this.props.board.grid.map(function(el) {
+        var rowContents = el.map(function(el2) {
+          return (
+            <TileComponent/>
+          )
+        });
+        return (
+          <div className="row">
+            {rowContents}
+          </div>
+        )
+      })
+
+      return (
+        <div className="grid">
+         {rows}
+        </div>
+      )
+    }
+  })
+
+  var TileComponent = React.createClass({
+    render: function() {
+      return (
+        <span> T </span>
+      )
+    }
+  })
+
+  React.render(
+    <GameComponent />,
+    document.getElementById('container')
+  )
 
 
 })();
